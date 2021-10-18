b7b09a0de1894a6ddb18ae3051436c68cf20fdba
Tycho Menting <Tycho.Menting@tomtom.com>
2021-10-18 12:59:53 +0000

    Merged PR 15381: IVI-3097: Intake of new dark UI.
    Intake of new dark UI.

--- buildSrc/src/main/kotlin/com/tomtom/ivi/buildsrc/environment/Versions.kt
+++ buildSrc/src/main/kotlin/com/tomtom/ivi/buildsrc/environment/Versions.kt
@@ -36,7 +36,7 @@ object Versions {
     const val MIN_SDK = 27
     const val TARGET_SDK = 27
 
-    const val TOMTOM_ANDROID_TOOLS = "0.0.202"
+    const val TOMTOM_ANDROID_TOOLS = "0.0.220"
 
     // Automotive
     const val ANDROID_CAR = "0.0.12"
