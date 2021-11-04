96806bc730ae948383d952cc58b3320590217ed4
Azure DevOps <ivi_dev@groups.tomtom.com>
2021-11-04 13:51:41 +0000

    Last integrated version of IndiGO platform: "1.0.2678"

--- buildSrc/src/main/kotlin/com/tomtom/ivi/buildsrc/environment/Versions.kt
+++ buildSrc/src/main/kotlin/com/tomtom/ivi/buildsrc/environment/Versions.kt
@@ -50,5 +50,5 @@ object Versions {
     const val IVI_COMMUNICATIONS_SDK = "0.1.72"
 
     // IndiGO platform version is updated by the `generateIndigoLibrariesVersionFile` gradle task.
-    const val INDIGO_PLATFORM = "1.0.2677"
+    const val INDIGO_PLATFORM = "1.0.2678"
 }
