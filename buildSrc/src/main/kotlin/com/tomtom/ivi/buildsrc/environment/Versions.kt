f49bf2be4e8efc45e67ca8021964486acf2f7816
Michiel van den Brink <michiel.vandenbrink@tomtom.com>
2021-11-11 11:34:42 +0000

    Merged PR 16567: DEV: Updated tools lib to latest
    Updated tools lib to latest

--- buildSrc/src/main/kotlin/com/tomtom/ivi/buildsrc/environment/Versions.kt
+++ buildSrc/src/main/kotlin/com/tomtom/ivi/buildsrc/environment/Versions.kt
@@ -36,7 +36,7 @@ object Versions {
     const val MIN_SDK = 27
     const val TARGET_SDK = 27
 
-    const val TOMTOM_ANDROID_TOOLS = "0.0.240"
+    const val TOMTOM_ANDROID_TOOLS = "0.0.244"
 
     // Automotive
     const val ANDROID_CAR = "0.0.12"
