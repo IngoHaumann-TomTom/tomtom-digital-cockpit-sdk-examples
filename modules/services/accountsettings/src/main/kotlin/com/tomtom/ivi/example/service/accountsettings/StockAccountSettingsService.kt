073e05900564095513ded0f79df6376bf602ec3d
Ariel Moreno Aleman <ariel.moreno@tomtom.com>
2021-11-16 11:27:18 +0000

    IVI-4670: Move api_common_configuration to platform_framework_api_configuration_providers
    IVI-4670: Move api_common_configuration to platform_framework_api_configuration_providers
    https://jira.tomtomgroup.com/browse/IVI-4670
    
    api_common_configuration -> platform_framework_api_configuration_providers
    api_framework_configuration -> platform_framework_api_configuration_api
    core_debug_configuration -> platform_framework_api_configuration_debug

--- modules/services/accountsettings/src/main/kotlin/com/tomtom/ivi/example/service/accountsettings/StockAccountSettingsService.kt
+++ modules/services/accountsettings/src/main/kotlin/com/tomtom/ivi/example/service/accountsettings/StockAccountSettingsService.kt
@@ -11,7 +11,6 @@
 
 package com.tomtom.ivi.example.service.accountsettings
 
-import com.tomtom.ivi.api.framework.configuration.LongDynamicConfigurationKey
 import com.tomtom.ivi.api.service.settingsmanagement.LongSettingKey
 import com.tomtom.ivi.api.service.settingsmanagement.SettingScope
 import com.tomtom.ivi.api.service.settingsmanagement.SettingUpdateStrategy
@@ -20,6 +19,7 @@ import com.tomtom.ivi.example.account.DynamicConfiguration.onlineLoginValidPerio
 import com.tomtom.ivi.example.account.DynamicConfiguration.settingKeyPrefix
 import com.tomtom.ivi.example.common.account.Account
 import com.tomtom.ivi.example.serviceapi.accountsettings.AccountSettingsServiceBase
+import com.tomtom.ivi.platform.framework.api.configuration.api.LongDynamicConfigurationKey
 import com.tomtom.ivi.platform.framework.api.ipc.iviservice.IviServiceHostContext
 import kotlinx.serialization.json.Json
 import java.time.Instant
