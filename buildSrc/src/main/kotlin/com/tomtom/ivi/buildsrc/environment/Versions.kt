7991dadecae09163e91cdf06baa5ce7f9503f6f1
Michiel van den Brink <michiel.vandenbrink@tomtom.com>
2021-11-30 20:41:14 +0000

    Merged PR 17150: DEV: Intake tools 0.0.271
    Intake tools 0.0.271

--- buildSrc/src/main/kotlin/com/tomtom/ivi/buildsrc/environment/Versions.kt
+++ buildSrc/src/main/kotlin/com/tomtom/ivi/buildsrc/environment/Versions.kt
@@ -36,7 +36,7 @@ object Versions {
     const val MIN_SDK = 27
     const val TARGET_SDK = 27
 
-    const val TOMTOM_ANDROID_TOOLS = "0.0.268"
+    const val TOMTOM_ANDROID_TOOLS = "0.0.271"
 
     // Automotive
     const val ANDROID_CAR = "0.0.12"
@@ -50,5 +50,5 @@ object Versions {
     const val IVI_COMMUNICATIONS_SDK = "0.1.76"
 
     // IndiGO platform version is updated by the `generateIndigoLibrariesVersionFile` gradle task.
-    const val INDIGO_PLATFORM = "1.0.2838"
+    const val INDIGO_PLATFORM = "1.0.2844"
 }
