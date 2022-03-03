7c0325f5a733ca14a2911ab5aa9780c03b9386a0
Valerio Pilo <Valerio.Pilo@tomtom.com>
2022-03-03 15:39:03 +0000

    Merged PR 20104: IVI-6713: Add Now Playing media example
    New example app showing how to make a media widget.
    
    ![androidScreenshot-2022-03-03.16.23.17.png](https://dev.azure.com/tomtomweb/815cc923-fd52-415e-b765-bc4a05b806a3/_apis/git/repositories/0e3185be-f7ab-43e1-9595-7832fc0b4196/pullRequests/20104/attachments/androidScreenshot-2022-03-03.16.23.17.png)

---
+++ examples/media/miniplayer/README.md
@@ -0,0 +1,28 @@
+# Module examples_media_miniplayer
+
+## Example custom media mini player
+
+This is an example demonstrating how to control media playback with a customized UI visible when
+not browsing for media in the stock media player UI.
+The stock media player UI uses a process bar to display what is currently being played, but it can
+be disabled when necessary: for example when media controls are always present in the UI.
+
+The example demonstrates concepts explained in the following guides from the TomTom IndiGO
+documentation:
+
+- [Configure the Media Plugins](https://developer.tomtom.com/tomtom-indigo/documentation/tutorials-and-examples/media/configure-the-media-plugins)
+- [Customize a media source](https://developer.tomtom.com/tomtom-indigo/documentation/tutorials-and-examples/media/customize-a-media-source)
+
+To be able to try the customizations in this example, other than installing it on a device, it is
+also necessary to compile and install the example module [examples_media_source]: that example
+contains the media source customized by this example. Trying another example or the template app
+will make the differences clearer.
+
+## System UI integration note
+
+This example only demonstrates how to make a new media playback UI; it does not provide an example
+on how to place this UI in a different part of the screen, or on another display.
+
+To accomplish those kinds of modifications, the
+[System UI guide](https://developer.tomtom.com/tomtom-indigo/documentation/development/system-ui)
+contains more information about customizing the UI and create new panel types to integrate in it.
---
+++ examples/media/miniplayer/src/main/kotlin/com/example/ivi/example/media/miniplayer/ExampleMiniPlayerFrontendBuilder.kt
@@ -0,0 +1,78 @@
+/*
+ * Copyright © 2022 TomTom NV. All rights reserved.
+ *
+ * This software is the proprietary copyright of TomTom NV and its subsidiaries and may be
+ * used for internal evaluation purposes or commercial use strictly subject to separate
+ * license agreement between you and TomTom NV. If you are the licensee, you are only permitted
+ * to use this software in accordance with the terms of your license agreement. If you are
+ * not the licensee, you are not authorized to use this software in any manner and should
+ * immediately return or destroy it.
+ */
+
+package com.example.ivi.example.media.miniplayer
+
+import com.tomtom.ivi.appsuite.media.api.common.frontend.MediaConfiguration
+import com.tomtom.ivi.appsuite.media.api.common.frontend.MediaPolicyFrontendExtension
+import com.tomtom.ivi.appsuite.media.api.common.frontend.policies.MediaTaskPanelPolicy
+import com.tomtom.ivi.appsuite.media.api.common.frontend.policies.PolicyProvider
+import com.tomtom.ivi.platform.frontend.api.common.frontend.Frontend
+import com.tomtom.ivi.platform.frontend.api.common.frontend.FrontendBuilder
+import com.tomtom.ivi.platform.frontend.api.common.frontend.FrontendContext
+
+/**
+ * This [FrontendBuilder] uses the [MediaPolicyFrontendExtension]s from the IVI Gradle configuration
+ * of the media frontend to customize the media sources exactly as the stock media player UI does.
+ *
+ * It is convenient when it is only necessary to create a new visual look for the "currently
+ * playing media" panel, or when moving it to a different part of the TomTom IndiGO system UI.
+ *
+ * For any deeper levels of customization, such as making a whole new media player UI, it is
+ * necessary to carefully evaluate what to re-use from the stock [MediaConfiguration]. For
+ * instance, the [MediaTaskPanelPolicy] provided by a [PolicyProvider] is probably not reusable
+ * in that case.
+ */
+internal class ExampleMiniPlayerFrontendBuilder : FrontendBuilder() {
+
+    override fun build(frontendContext: FrontendContext): Frontend {
+        val extensions = getFrontendExtensionsByType<MediaPolicyFrontendExtension>()
+        return ExampleMiniPlayerFrontend(
+            frontendContext,
+            MediaConfiguration(
+                extensions.asSourceToPolicyProviderMap(),
+                extensions.asFallbackPolicyProvider()
+            )
+        )
+    }
+
+    companion object {
+
+        /**
+         * Converts [MediaPolicyFrontendExtension]s to a map of source-specific [PolicyProvider]s.
+         */
+        private fun Collection<MediaPolicyFrontendExtension>.asSourceToPolicyProviderMap() =
+            filter { it.sourceId != null }
+                .run {
+                    // Only one media policy frontend extension may be specified per source.
+                    check(size == distinctBy { it.sourceId!! }.size) {
+                        "A single source was specified more than one time, " +
+                            "sources=${map { it.sourceId }}"
+                    }
+
+                    associateBy({ it.sourceId!! }, { it.policyProvider })
+                }
+
+        /**
+         * Scans [MediaPolicyFrontendExtension]s for the fallback [PolicyProvider].
+         */
+        private fun Collection<MediaPolicyFrontendExtension>.asFallbackPolicyProvider() =
+            filter { it.sourceId == null }
+                .run {
+                    check(size == 1) {
+                        "Exactly one fallback media policy frontend extension can be specified! " +
+                            "detected=$size"
+                    }
+
+                    single().policyProvider
+                }
+    }
+}
